b4bbefc3824da08b651080f8422b02f9b13298b8
Jym-lab <jym9809@gmail.com>
2022-01-19 14:08:16 +0900

    m

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: yjoo <yjoo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 13:39:50 by yjoo              #+#    #+#             */
-/*   Updated: 2022/01/18 22:41:07 by yjoo             ###   ########.fr       */
+/*   Updated: 2022/01/19 14:07:39 by yjoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -117,24 +117,22 @@ t_list	*find_node(t_list **h_node, int fd)
 
 char	*get_next_line(int fd)
 {
-	static t_list	*h_node;
+	static t_list	*h_node = 0;
 	t_list			*cur_node;
 	char			*line;
 
 	if (fd < 0 || BUFFER_SIZE < 0 || fd > 256)
-		return ("");
+		return (NULL);
 	cur_node = find_node(&h_node, fd);
 	cur_node->buffer = read_buffer(cur_node->fd, cur_node->buffer);
 	if (cur_node->buffer == NULL)
 	{
 		free_node(&h_node, cur_node);
-		return ("");
+		return (NULL);
 	}
 	line = get_line(cur_node->buffer);
 	cur_node->buffer = save_buffer(cur_node->buffer);
 	if (cur_node->buffer == NULL)
 		free_node(&h_node, cur_node);
-	if (line == NULL)
-		return ("");
 	return (line);
 }
